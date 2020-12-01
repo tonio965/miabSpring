f8e1e039904d61addad4f3731aa617a93403d89a
zwykły tomek <39235399+tonio965@users.noreply.github.com>
2020-12-01 23:50:54 +0100

    jakies smieciowe testy 3

--- src/test/java/com/miab/miab/MiabApplicationTests.java
+++ src/test/java/com/miab/miab/MiabApplicationTests.java
@@ -6,8 +6,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.miab.model.Grup;
 import com.miab.model.User;
+import com.miab.repository.GroupFacade;
+import com.miab.repository.RepositoryFacade;
 import com.miab.services.AppplicationService;
 import com.miab.services.UserService;
 
@@ -19,23 +23,37 @@ class MiabApplicationTests {
 	
 	
 	@Autowired
-	private UserService userService;
+	private RepositoryFacade userService;
+	
+	@Autowired
+	private GroupFacade groupService;
 	
 	@Test
 	void addUserToDb() {
-		int i = userService.returnAllUsers();
-		userService.createUser("test1", "test1", "test1", "test1");
-		int j = userService.returnAllUsers();
-		assertThat(j).isGreaterThan(i);
+		int i = userService.findAll().size();
+		User u = new User("tomek", "tomek", "tomek", "tomek");
+		userService.save(u);
+		int j = userService.findAll().size();
+		assertThat(j>i).isEqualTo(true);
 	}
 	
 	@Test
 	void deletesUser() {
-		
-		userService.createUser("test2", "test2", "test2", "test2");
-		int i = userService.returnAllUsers();
-		userService.deleteUser("test2");
-		int j = userService.returnAllUsers();
-		assertThat(j).isLessThan(i);
+		int i = userService.findAll().size();
+		userService.deleteByUsername("tomek");
+		int j = userService.findAll().size();
+		assertThat(j<i).isEqualTo(true);
 	}
+	
+	@Test
+	void createGroup() {
+		int i = groupService.findAll().size();
+		Grup g = new Grup(1, "gr1");
+		groupService.save(g);
+		int j = groupService.findAll().size();
+		assertThat(j>i).isEqualTo(true);
+	}
+	
+	
+	
 }
--- src/test/java/com/miab/miab/UserServiceTest.java
+++ src/test/java/com/miab/miab/UserServiceTest.java
@@ -4,26 +4,27 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.miab.model.User;
+import com.miab.repository.RepositoryFacade;
 import com.miab.services.UserService;
 
 public class UserServiceTest {
 	
 	@Autowired
-	private UserService userService;
+	private RepositoryFacade userService;
 	
 	@Test
 	void addUserToDb() {
-		int i = userService.returnAllUsers();
-		User u = new User("test1", "test1", "test1", "test1");
-		int j = userService.returnAllUsers();
+		int i = userService.findAll().size();
+		User u = new User("tomek", "tomek", "tomek", "tomek");
+		int j = userService.findAll().size();
 		assertThat(j>i).isEqualTo(true);
 	}
 	
 	@Test
 	void deletesUser() {
-		int i = userService.returnAllUsers();
+		int i = userService.findAll().size();
 		User u = new User("test2", "test2", "test2", "test2");
-		int j = userService.returnAllUsers();
+		int j = userService.findAll().size();
 		assertThat(j<i).isEqualTo(true);
 	}
 	
